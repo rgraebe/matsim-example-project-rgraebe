6f57378037f86c0102e7a22fa8a191b9dbc8423b
Ruan J. Gräbe <rgraebe@vsp.tu-berlin.de>
2022-11-29 12:43:43 +0100

    Adv class: interfaces

---
+++ src/main/java/org/matsim/project/Run31Oct22b.java
@@ -0,0 +1,46 @@
+package org.matsim.project;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Module;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Injector;
+import org.matsim.core.controler.OutputDirectoryHierarchy;
+
+class Run31Oct22b {
+
+
+    public static void main(String[] args) {
+        Config config = ConfigUtils.loadConfig( "scenarios/equil/config.xml" );
+        config.controler().setOverwriteFileSetting( OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists );
+        config.controler().setLastIteration( 2 );
+
+        // use the matsim Module!
+        Module module  = new org.matsim.core.controler.AbstractModule() {
+            @Override // auto-generated
+            public void install() {
+                bind( Abc.class ).to( AbcImpl.class );
+            }
+        };
+
+        com.google.inject.Injector injector = Injector.createInjector( config, module );
+
+        Abc abc = injector.getInstance( Abc.class );
+        abc.doSomething();
+
+    }
+
+    interface Abc { // manually added - my own interface...
+        void doSomething();
+    }
+
+    private static class AbcImpl implements Abc {
+
+        @Override public void doSomething(){
+            System.out.println("executing doSomething of AbcImpl");
+        }
+
+    }
+
+
+}
